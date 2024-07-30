f7d140d24fdf7ef7da74148c66e1ccb1de5ef9ad
prthomas808 <prthomas808@gmail.com>
2024-07-30 09:04:38 -0400

    Navigation bar and sample message created

---
+++ ChatApp/Inbox/Views/InboxView.swift
@@ -0,0 +1,18 @@
+//
+//  InboxView.swift
+//  ChatApp
+//
+//  Created by Pedro Thomas on 7/30/24.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InboxView()
+}
