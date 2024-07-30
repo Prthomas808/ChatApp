85744c6b46110e4e7f23e03c5aa867795c002c83
prthomas808 <prthomas808@gmail.com>
2024-07-30 13:11:26 -0400

    newMessageView created

---
+++ ChatApp/NewMessage/Views/NewMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMessageView.swift
+//  ChatApp
+//
+//  Created by Pedro Thomas on 7/30/24.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMessageView()
+}
